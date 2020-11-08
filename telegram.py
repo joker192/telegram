0f0311273cd1bf448c53ee5c06151c7ac5ed286f
joker192 <73433769+joker192@users.noreply.github.com>
2020-11-08 23:04:25 +0330

    Create telegram.py

---
+++ telegram.py
@@ -0,0 +1,17 @@
+import os 
+red='\033[31m'
+green='\033[32m'
+orange='\033[33m'
+print(f"{orange}============================")
+print(f"{orange}     made by bad_boy        ")
+print(f"{orange}============================")
+print(f"{red}[1]start the hack")
+print("[2]exit")
+joker=int(input("==>"))
+if joker == 1:
+    king=int(input("Enter Target phone Number : "))
+    print(f"{green}STARTED!")
+    print("telegram.login ==> feaild\n" * 300 + "telegram.login ==> connect\n" * 30 + "code: 34890")
+elif joker == 2:
+    os.system("clear")
+    print("exiting!!")
